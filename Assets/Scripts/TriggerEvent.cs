a6df17dee20e68eab34c5c4b26d07f3375542e36
alanfeiyuchang <alanfeiyuchang@gmail.com>
2022-02-26 19:57:56 -0800

    Update TriggerEvent.cs

--- Assets/Scripts/TriggerEvent.cs
+++ Assets/Scripts/TriggerEvent.cs
@@ -7,10 +7,21 @@ public class TriggerEvent : MonoBehaviour
 {
     [SerializeField] private UnityEvent _action;
     [SerializeField] private Animation _animation;
+    [SerializeField] private bool _onlyTriggerOnce = true;
+
+    private bool _triggered = false;
     private void OnTriggerEnter(Collider other)
     {
+        if (_triggered)
+            return;
         if (other.CompareTag("Player"))
+        {
             _action.Invoke();
+            if (_onlyTriggerOnce)
+            {
+                _triggered = true;
+            }
+        }
     }
 
     public void PlayHeadmonsterAnim()
